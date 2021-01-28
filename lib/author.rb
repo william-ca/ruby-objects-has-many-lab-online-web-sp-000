026e441efe8cbe6b0a5c205a7f2c688b394657b4
Will Andre <wandre94@gmail.com>
2021-01-28 21:30:22 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,7 @@
 class Author
 
   attr_accessor :name, :posts
-  @@post_count = 0
+
 
   def initialize(name)
     @name = name
@@ -13,14 +13,12 @@ class Author
   end
 
   def add_post(post)
-    @@post_count+=1
     @posts << Post.new(post)
     post.author = self
 
   end
 
   def add_post_by_title(post_title)
-    @@post_count+=1
     new_post = Post.new(post_title)
     @posts << new_post
     new_post.author = self
